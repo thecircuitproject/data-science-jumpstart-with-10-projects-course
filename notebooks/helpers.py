14718275b94b724ae230a54c31bf0698b124ffee
Jose Carlos <thecircuitproject@gmail.com>
2024-01-02 18:41:02 -0600

    Project 2: Ch 3 with Polars

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -1,21 +1,22 @@
 import numpy as np
+import polars as pl
 
 
-def jitter(df, col, amt=.5):
-    return df[col] + np.random.random(len(df))*amt - amt/2
+def jitter(df, col, amt=0.5):
+    return df[col] + np.random.random(len(df)) * amt - amt / 2
 
 
-def debug(df, extra=''):
-    print(f'{extra} {df.shape=}')
+def jitter_polars(df, col, amt=0.5):
+    return df.select(pl.col(col)).to_series()
+
+
+def debug(df, extra=""):
+    print(f"{extra} {df.shape=}")
     return df
 
 
-def limit_n(df, col, n=20, other='Other'):
-    top = (df
-           [col]
-           .value_counts()
-           )
+def limit_n(df, col, n=20, other="Other"):
+    top = df[col].value_counts()
 
     topn = top.index[:n]
     return df[col].where(df[col].isin(topn), other)
-
