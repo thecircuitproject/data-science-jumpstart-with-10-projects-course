09ec5b89a7e77b302161317ed221fe57c7a6f3f6
Jose Carlos <thecircuitproject@gmail.com>
2024-01-02 19:05:33 -0600

    Added type hints to helpers.py

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -1,21 +1,24 @@
 import numpy as np
 import polars as pl
+import pandas as pd
 
 
-def jitter(df, col, amt=0.5):
+def jitter(df: pd.DataFrame, col: str, amt: int = 0.5) -> pd.DataFrame:
     return df[col] + np.random.random(len(df)) * amt - amt / 2
 
 
-def jitter_polars(df, col, amt=0.5):
+def jitter_polars(df: pl.DataFrame, col: str, amt: int = 0.5) -> pl.DataFrame:
     return df.select(pl.col(col)).to_series()
 
 
-def debug(df, extra=""):
+def debug(df: pd.DataFrame, extra: str = "") -> pd.DataFrame:
     print(f"{extra} {df.shape=}")
     return df
 
 
-def limit_n(df, col, n=20, other="Other"):
+def limit_n(
+    df: pd.DataFrame, col: str, n: int = 20, other: str = "Other"
+) -> pl.DataFrame:
     top = df[col].value_counts()
 
     topn = top.index[:n]
