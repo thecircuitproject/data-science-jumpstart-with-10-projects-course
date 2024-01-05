d95804cb48a498b17bd639e7426aa3e10be60e3b
Jose Carlos <thecircuitproject@gmail.com>
2024-01-04 19:54:13 -0600

    Added new function limit_n_pl for Polars dataframes

--- notebooks/helpers.py
+++ notebooks/helpers.py
@@ -16,10 +16,23 @@ def debug(df: pd.DataFrame, extra: str = "") -> pd.DataFrame:
     return df
 
 
-def limit_n(
-    df: pd.DataFrame, col: str, n: int = 20, other: str = "Other"
-) -> pl.DataFrame:
+def limit_n(df: pd.DataFrame, col: str, n: int = 20, other: str = "Other") -> pd.Series:
     top = df[col].value_counts()
 
     topn = top.index[:n]
     return df[col].where(df[col].isin(topn), other)
+
+
+def limit_n_pl(
+    df: pl.DataFrame, col: str, n: int = 20, other: str = "Other"
+) -> pl.DataFrame:
+    top_pl = df[col].value_counts(sort=True)
+
+    topn_pl = top_pl[:n]
+
+    return df.with_columns(
+        pl.when(pl.col(col).is_in(topn_pl[col]))
+        .then(pl.col(col))
+        .otherwise(pl.lit("Other"))
+        .alias(col)
+    )[col]
